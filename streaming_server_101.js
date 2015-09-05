c03bc7716a7c5c22b855897b8d6194307b4869e1
am2100 <jimnoble@xjjz.co.uk>
2015-09-05 10:48:45 +0100

    Create streaming server

---
+++ streaming_server_101.js
@@ -0,0 +1,49 @@
+/* streaming_server_101.js */
+
+var http = require('http'),
+    fs   = require('fs'),
+    path = require('path'),
+    host = '127.0.0.1',
+    port = '9000';
+
+var mimes = {
+  ".html" : "text/html",
+  ".css"  : "text/css",
+  ".js"   : "text/js",
+  ".gif"  : "image/gif",
+  ".jpg"  : "image/jpeg",
+  ".png"  : "image/png"
+}
+
+var server = http.createServer(function(req, res) {
+    var filepath = (req.url === '/') ? ('./index.html') : ('.' + req.url);
+    var contentType = mimes[path.extname(filepath)];
+    // Check to see if the file exists
+    fs.exists(filepath, function(file_exists){
+      if (file_exists){
+        // Read and serve
+        // fs.readFile(filepath, function(error, content){
+          // if (error){
+            // res.writeHead(500);
+            // res.end();
+          // } else {
+            // res.writeHead(200, { 'Content-Type' : contentType });
+            // res.end(content, 'utf-8');
+          // }
+        //})
+        res.writeHead(200, {'Content-Type' : contentType});
+        var streamFile = fs.createReadStream(filepath).pipe(res);
+        
+        streamFile.on('error', function(){
+          res.writeHead(500);
+          res.end();
+        })
+      } else {
+        res.writeHead(404);
+        res.end('Sorry, file not found!');
+      }
+    })
+  }).listen(port, host, function(){
+    console.log('Server running on http://' + host + ':' + port);
+})
+
