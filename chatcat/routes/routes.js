c4de2894dd2da94c0cc981e16e856ef692618632
am2100 <jimnoble@xjjz.co.uk>
2015-09-28 20:31:30 +0100

    Add routes module

---
+++ chatcat/routes/routes.js
@@ -0,0 +1,22 @@
+module.exports = function(express, app){
+  var router = express.Router();
+
+  router.get('/', function(req, res, next){
+    res.render('index', {title: 'Welcome to ChatCat'});
+  })
+
+  router.get('/chatrooms', function(req, res, next){
+    res.render('chatrooms', {title: 'Chatrooms'});
+  })
+  
+  router.get('/setcolor', function(req, res, next){
+      req.session.favColor = 'Red';
+        res.send('Setting favColor');
+  })
+
+  router.get('/getcolor', function(req, res, next){
+      res.send('favColor: ' + (req.session.favColor === undefined?'Not found':req.session.favColor));
+  })
+
+  app.use('/', router);
+}
