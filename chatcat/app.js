0d04c9b628724134f037aad461413d0e05414f18
am2100 <jimnoble@xjjz.co.uk>
2015-10-07 12:27:18 +0100

    Add properties to session object.

--- chatcat/app.js
+++ chatcat/app.js
@@ -11,15 +11,27 @@ app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(session({secret: 'catscanfly'}));
 
 var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development'){
   // development specific settings
-  app.use(session({secret: config.sessionSecret}));
+  app.use(session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false
+  }));
 } else {
   // production specific settings
+  app.use(session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    store: new ConnectMongo({
+      url: config.dbURL,
+      stringify: true
+    })
+  }))
 }
 require('./routes/routes.js')(express, app);
 
